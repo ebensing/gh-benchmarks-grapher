904baaf62de7bed657e64f931a4d3e4c7a598f0d
EJ Bensing <ebensing@rm-dash-r.com>
2013-08-19 11:59:54 -0700

    check to make sure save_loc exists

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var ncp = require('ncp').ncp;
 var utils = require('util');
 var jade = require('jade');
+var mkdirp = require('mkdirp');
 
 // helper function to get object property by string
 Object.byString = function(o, s) {
@@ -80,36 +81,43 @@ exports.buildGraphs = function (runs, job, repo_loc, callback) {
   // save this data to a json file for use on github
   var writeObj = { data : data, charts : charts };
   var saveLoc = utils.format("%s/%s/data.json", repo_loc, job.saveLoc);
-  fs.writeFileSync(saveLoc, JSON.stringify(writeObj));
+  // make sure the save location exists
+  mkdirp(repo_loc + "/" + job.saveLoc, function (err) {
+    if (err) {
+      return callback(err);
+    }
+    fs.writeFileSync(saveLoc, JSON.stringify(writeObj));
 
-  // generate the html page
-  var locals = {};
-  locals.projectName = job.projectName;
-  locals.title = job.title;
-  locals.charts = charts;
+    // generate the html page
+    var locals = {};
+    locals.projectName = job.projectName;
+    locals.title = job.title;
+    locals.charts = charts;
 
-  // get system information, should be the same for all runs
-  if (runs.length) {
-    locals.sysinfo = runs[0].sysinfo;
-  }
+    // get system information, should be the same for all runs
+    if (runs.length) {
+      locals.sysinfo = runs[0].sysinfo;
+    }
 
-  fs.readFile(__dirname + '/static/index.jade', 'utf-8', function (err, data) {
-    if (err) return callback(err);
-
-    var fn = jade.compile(data);
-    var html = fn(locals);
-    var saveLocation = utils.format("%s/%s/index.html", repo_loc, job.saveLoc);
-    fs.writeFile(saveLocation, html, function (err) {
+    fs.readFile(__dirname + '/static/index.jade', 'utf-8', function (err, data) {
       if (err) return callback(err);
-      // copy the dependencies into the repo
-      var saveDir = utils.format("%s/%s/", repo_loc, job.saveLoc);
-      ncp(__dirname + "/static", saveDir, { clobber : true }, function (err) {
-        var files = ["index.html", "data.json", "graphs.js", "style.css", "bootstrap/"];
-        files = files.map(function (item) {
-          return utils.format("%s/%s", job.saveLoc, item);
+
+      var fn = jade.compile(data);
+      var html = fn(locals);
+      var saveLocation = utils.format("%s/%s/index.html", repo_loc, job.saveLoc);
+      fs.writeFile(saveLocation, html, function (err) {
+        if (err) return callback(err);
+        // copy the dependencies into the repo
+        var saveDir = utils.format("%s/%s/", repo_loc, job.saveLoc);
+        ncp(__dirname + "/static", saveDir, { clobber : true }, function (err) {
+          var files = ["index.html", "data.json", "graphs.js", "style.css", "bootstrap/"];
+          files = files.map(function (item) {
+            return utils.format("%s/%s", job.saveLoc, item);
+          });
+          callback(err, files);
         });
-        callback(err, files);
       });
     });
   });
+
 }
